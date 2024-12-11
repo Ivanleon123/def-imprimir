11741ff6ea7edc54cfaa18e0fd56852cca5eb614
Ivanleon123 <imunyoz20974@iesjoanramis.org>
2024-12-11 09:16:40 +0100

    Update def imprimir_sequencia():.py

--- def imprimir_sequencia():.py
+++ def imprimir_sequencia():.py
@@ -2,13 +2,10 @@ def imprimir_sequencia():
     """Imprimeix la sèrie de números de l'1 al 15."""
     for i in range(1, 16):
         print(i, end=' ')
-    print()  # Salta a la següent línia
+    print()  
 
 def main():
-    # Imprimir la sèrie cinc vegades
     for _ in range(5):
         imprimir_sequencia()
-
-# Executar el programa
 if __name__ == "__main__":
     main()
